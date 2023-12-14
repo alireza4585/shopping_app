ec26be1af1f2ba6da6e2454cde896ff37392ab33
Alireza Mohammadian <alirezamohammadian@Alirezas-Mac-mini.local>
2023-12-13 19:13:08 -0800

    producr Responsive UI

--- lib/Screens/product_detail_screen.dart
+++ lib/Screens/product_detail_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopping_app/Screens/home.dart';
 import 'package:shopping_app/constants/colors.dart';
 
+// ignore: must_be_immutable
 class ProductDetailScreen extends StatefulWidget {
   int index;
   ProductDetailScreen({super.key, required this.index});
@@ -26,7 +28,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
 
     return Scaffold(
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 10.h),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -34,21 +36,21 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
               'pice',
               style: TextStyle(
                 color: black,
-                fontSize: 19,
+                fontSize: 19.sp,
                 fontWeight: FontWeight.bold,
               ),
             ),
             Container(
-              width: 190,
-              height: 50,
+              width: 190.w,
+              height: 50.h,
               decoration: BoxDecoration(
                 color: black,
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(10.r),
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Icon(
+                  const Icon(
                     Icons.shopping_cart_outlined,
                     color: Colors.white,
                   ),
@@ -57,7 +59,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                     style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
-                      fontSize: 15,
+                      fontSize: 15.sp,
                     ),
                   ),
                 ],
@@ -71,7 +73,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
         child: CustomScrollView(
           slivers: [
             SliverAppBar(
-              flexibleSpace: FlexibleSpaceBar(
+              flexibleSpace: const FlexibleSpaceBar(
                   // background: Image.asset(
                   //   'images/${get_images[indexx]}',
                   //   fit: BoxFit.cover,
@@ -80,46 +82,46 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
               expandedHeight: 400,
               backgroundColor: backgroundColor,
               bottom: PreferredSize(
+                preferredSize: Size.fromHeight(10.h),
                 child: Container(
-                  height: 20,
+                  height: 20.h,
                   width: double.infinity,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(70),
-                      topRight: Radius.circular(70),
+                      topLeft: Radius.circular(70.r),
+                      topRight: Radius.circular(70.r),
                     ),
                   ),
                   child: Column(
                     children: [
-                      SizedBox(height: 7),
+                      SizedBox(height: 7.h),
                       Container(
-                        height: 4,
-                        width: 100,
+                        height: 4.h,
+                        width: 100.w,
                         decoration: BoxDecoration(
                           color: Colors.black,
                           borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(70),
-                            topRight: Radius.circular(70),
+                            topLeft: Radius.circular(70.r),
+                            topRight: Radius.circular(70.r),
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
-                preferredSize: Size.fromHeight(10),
               ),
               automaticallyImplyLeading: false,
               actions: [
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  padding: EdgeInsets.symmetric(horizontal: 15.w),
                   child: CircleAvatar(
-                    backgroundColor: Color.fromRGBO(250, 250, 250, 0.6),
-                    radius: 18,
+                    backgroundColor: const Color.fromRGBO(250, 250, 250, 0.6),
+                    radius: 18.r,
                     child: Icon(
                       Icons.favorite_border,
                       color: black,
-                      size: 25,
+                      size: 25.w,
                     ),
                   ),
                 ),
@@ -128,63 +130,64 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                 onTap: () {
                   Navigator.of(context).pop(
                     MaterialPageRoute(
-                      builder: (BuildContext context) => Home_Screen(),
+                      builder: (BuildContext context) => const Home_Screen(),
                     ),
                   );
                 },
                 child: CircleAvatar(
-                  backgroundColor: Color.fromRGBO(250, 250, 250, 0.6),
-                  radius: 18,
+                  backgroundColor: const Color.fromRGBO(250, 250, 250, 0.6),
+                  radius: 18.r,
                   child: Icon(
                     Icons.arrow_back,
                     color: Colors.black,
-                    size: 25,
+                    size: 25.w,
                   ),
                 ),
               ),
             ),
             SliverPadding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
+              padding: EdgeInsets.symmetric(horizontal: 15.w),
               sliver: SliverToBoxAdapter(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(height: 20),
+                    SizedBox(height: 20.h),
                     Text(
                       'name',
                       style: TextStyle(
-                        fontSize: 24,
+                        fontSize: 24.sp,
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 12),
+                    SizedBox(height: 12.h),
                     Row(
                       children: [
-                        Icon(
+                        const Icon(
                           Icons.star,
                           color: Colors.amber,
                         ),
-                        SizedBox(width: 6),
+                        SizedBox(width: 6.w),
                         Text(
                           '4.3',
                           style: TextStyle(
-                            fontSize: 15,
+                            fontSize: 15.sp,
                             color: Colors.grey.shade600,
                             fontWeight: FontWeight.w600,
                           ),
                         ),
                       ],
                     ),
-                    SizedBox(height: 20),
-                    Container(
-                      height: 70,
+                    SizedBox(height: 20.h),
+                    SizedBox(
+                      height: 70.h,
                       child: ListView.builder(
                         itemCount: 4,
                         scrollDirection: Axis.horizontal,
                         itemBuilder: (context, index) {
                           return Padding(
-                            padding: EdgeInsets.only(left: index == 0 ? 0 : 24),
+                            padding:
+                                EdgeInsets.only(left: index == 0 ? 0 : 24.w),
                             child: GestureDetector(
                               onTap: () {
                                 setState(() {
@@ -192,15 +195,15 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                                 });
                               },
                               child: Container(
-                                width: 70,
-                                height: 70,
+                                width: 70.w,
+                                height: 70.h,
                                 decoration: BoxDecoration(
                                   border: Border.all(
-                                    width: 2,
+                                    width: 2.w,
                                     color:
                                         indexx == index ? black : Colors.grey,
                                   ),
-                                  borderRadius: BorderRadius.circular(10),
+                                  borderRadius: BorderRadius.circular(10.r),
                                   // image: DecorationImage(
                                   //   image: AssetImage(
                                   //     'images/${get_images[index]}',
@@ -214,24 +217,24 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                         },
                       ),
                     ),
-                    SizedBox(height: 25),
+                    SizedBox(height: 25.h),
                     Text(
                       'Select Size',
                       style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 18.sp,
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 15),
-                    Container(
-                      height: 35,
+                    SizedBox(height: 15.h),
+                    SizedBox(
+                      height: 35.h,
                       child: ListView.builder(
                         itemCount: 12,
                         scrollDirection: Axis.horizontal,
                         itemBuilder: (context, index) {
                           return Padding(
-                            padding: EdgeInsets.only(right: 11),
+                            padding: EdgeInsets.only(right: 11.w),
                             child: GestureDetector(
                               onTap: () {
                                 setState(() {
@@ -240,20 +243,20 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                               },
                               child: Container(
                                 alignment: Alignment.center,
-                                width: 35,
-                                height: 35,
+                                width: 35.w,
+                                height: 35.h,
                                 decoration: BoxDecoration(
                                   border: Border.all(
-                                    width: 1.2,
+                                    width: 1.2.w,
                                     color:
                                         indexxx == index ? black : Colors.grey,
                                   ),
-                                  borderRadius: BorderRadius.circular(10),
+                                  borderRadius: BorderRadius.circular(10.r),
                                 ),
                                 child: Text(
                                   '${size[index]}',
                                   style: TextStyle(
-                                    fontSize: 14,
+                                    fontSize: 14.sp,
                                     color: Colors.black,
                                     fontWeight: FontWeight.bold,
                                   ),
@@ -264,18 +267,18 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                         },
                       ),
                     ),
-                    SizedBox(height: 20),
+                    SizedBox(height: 20.h),
                     Text(
                       'Explain',
                       style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 18.sp,
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 10),
+                    SizedBox(height: 10.h),
                     Padding(
-                      padding: const EdgeInsets.only(bottom: 20),
+                      padding: EdgeInsets.only(bottom: 20.w),
                       child: Text(
                         "Get in the Queue. Get the goods.Create or sign in to your free Nike Member account for a chance to buy this product the moment it's releasedThe Air Jordan 1 Mid brings full-court style and premium comfort to an iconic look. Its Air-Sole unit cushions play on the hardwood, while the padded collar gives you a supportive feel.Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity.Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity.Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity.Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity.Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity",
                         style: TextStyle(
