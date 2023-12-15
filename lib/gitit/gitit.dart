9956bdbcb53ff4d364ee67f1909a7492929baacf
Alireza Mohammadian <alirezamohammadian@Alirezas-Mac-mini.local>
2023-12-14 19:36:01 -0800

    intialize gitint

---
+++ lib/gitit/gitit.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+
+var locator = GetIt.instance;
+Future<void> getItInit() async {
+  _initDatasoruces();
+
+  _initRepositories();
+}
+
+void _initDatasoruces() {}
+
+void _initRepositories() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopping_app/Screens/home.dart';
+import 'package:shopping_app/gitit/gitit.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await getItInit();
   runApp(const MyApp());
 }
 
