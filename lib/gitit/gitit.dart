f9d9279ff3e6a668761af2b8b4a7a425d3d07d40
Alireza Mohammadian <alirezamohammadian@Alirezas-Mac-mini.local>
2024-01-03 06:45:27 -0800

    authentication datasource and repository

--- lib/gitit/gitit.dart
+++ lib/gitit/gitit.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shopping_app/data/datasource/authentication_datasource.dart';
 import 'package:shopping_app/data/datasource/banner_datasource.dart';
 import 'package:shopping_app/data/datasource/product_detail_darasource.dart';
 import 'package:shopping_app/data/datasource/products_datasource.dart';
+import 'package:shopping_app/data/repository/authentication_repository.dart';
 import 'package:shopping_app/data/repository/banner_repository.dart';
 import 'package:shopping_app/data/repository/product_detail_repository.dart';
 import 'package:shopping_app/data/repository/products_repository.dart';
@@ -18,9 +21,13 @@ Future<void> getItInit() async {
 
 Future<void> _initComponents() async {
   locator.registerSingleton<Dio>(DioProvider.createDioWithoutHeader());
+  locator.registerSingleton<SharedPreferences>(
+      await SharedPreferences.getInstance());
 }
 
 Future<void> _initDatasoruces() async {
+  locator
+      .registerFactory<IAuthenticationDatasource>(() => AuthenticationRemote());
   locator.registerFactory<IBannerDatasource>(() => BannerRemoteDatasource());
   locator.registerFactory<IProductDatasource>(() => ProductRemoteDatasource());
   locator
@@ -28,6 +35,7 @@ Future<void> _initDatasoruces() async {
 }
 
 void _initRepositories() {
+  locator.registerFactory<IAuthRepository>(() => AuthenticationRepository());
   locator.registerFactory<IBannerRepository>(() => BannerRepository());
   locator.registerFactory<IProductRepository>(() => ProductRepository());
   locator.registerFactory<IProductDetailRepository>(
